c24843ce7d7f0550262b21dfcbfaacd2611139ad
Jonny-Long-1 <154447828+Jonny-Long-1@users.noreply.github.com>
2024-01-01 15:44:16 +0000

    Update Go_Slim_attribute_applier_1.R

--- 2ClassNetworkCreation/Go_Slim_attribute_applier_1.R
+++ 2ClassNetworkCreation/Go_Slim_attribute_applier_1.R
@@ -1,6 +1,6 @@
 #Use before GO_slim_attribute_applier
 
-slim_term <- "flower development" #Input your GO slim term
+slim_term <- "example GO term" #Input your GO slim term
 
 
 #Take in a csv file containing loci, their corresponding GO terms, and the GO slims assosciated with each
@@ -9,7 +9,7 @@ slim_term <- "flower development" #Input your GO slim term
 
 GOs <- read.csv("../raw_data/All_plant_GO_terms_0.975_csv.csv", header=TRUE) %>% 
   rename(GO_slim= GO.Slim.s.) %>% select(Locus,GO.term,GO.ID,category,GO_slim)  
-#Bi_proc <- GOs %>% filter(category == "proc") #Include only biological processes (maybe redundant)
+#Bi_proc <- GOs %>% filter(category == "proc") #Include any filters desired
 
 subset<- Bi_proc[grep(slim_term, Bi_proc$GO_slim),] #Search for your slim term of interest
 unique_subset <- unique(subset$Locus) #Include each locus in the list only once
